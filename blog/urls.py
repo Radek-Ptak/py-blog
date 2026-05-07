6518202d52caec2a6c880c6b80aca39dbfc6ee0e
Radoslaw Ptak <radek1026@interi.pl>
2026-05-07 20:18:53 +0200

    Implement comment editing and removal in py-blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,14 +4,24 @@ from .views import (
     index,
     PostDetailView,
     CommentaryCreateView,
+    CommentaryUpdateView,
+    CommentaryDeleteView,
 )
 
 app_name = "blog"
 urlpatterns = [
     path("", index, name="index"),
-    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
-    path("commentary/create",
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/comments/create/",
          CommentaryCreateView.as_view(),
-         name="commentary-create"
+         name="comment-create"
+         ),
+    path("comments/<int:pk>/update/",
+         CommentaryUpdateView.as_view(),
+         name="comment-update"
+         ),
+    path("comments/<int:pk>/delete/",
+         CommentaryDeleteView.as_view(),
+         name="comment-delete"
          ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -42,5 +42,51 @@ class PostDetailView(generic.DetailView):
 class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Commentary
     fields = ["content"]
-    success_url = reverse_lazy("blog:post-detail")
     template_name = "blog/commentary_form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post_id = self.kwargs["pk"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "blog:post-detail", kwargs={"pk": self.kwargs["pk"]}
+        )
+
+
+class CommentaryUpdateView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.UpdateView
+):
+    model = Commentary
+    fields = ["content"]
+    template_name = "blog/commentary_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "blog:post-detail", kwargs={"pk": self.get_object().post.pk}
+        )
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.user
+
+
+class CommentaryDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.DeleteView
+):
+    model = Commentary
+    template_name = "blog/commentary_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "blog:post-detail", kwargs={"pk": self.get_object().post.pk}
+        )
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.user
